279b7a3a78445447e753c14fd5289c8fd35ea958
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-03-13 23:16:14 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,18 @@
+  var problem = {
+    prompt: "Solve for x",
+    equation: "2x + 5 = 13",
+    answers: [
+      {
+        id: 0,
+        value: "4"
+      },
+      {
+        id: 1,
+        value: "5"
+      },
+      {
+        id: 2,
+        value: "6"
+      }
+    ]
+  };
