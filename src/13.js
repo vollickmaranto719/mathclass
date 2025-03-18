3eb781c4d2bd85d21eadb47397ae41c604b7ad83
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-03-18 04:49:52 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,14 @@
+MathClass = { 
+   add: function(a, b) {
+     return a + b;
+   },
+    substract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
+    }
+};
