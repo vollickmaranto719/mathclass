d9cdfe425ea4bcef53b8c245acd26a803600ac66
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-03-30 17:12:12 +0000

    Add 20.js

---
+++ src/20.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage: generate a random number between 1 and 10
+const randomNumber = getRandomInt(10);
+console.log(randomNumber); // Output will vary each time the code is run
