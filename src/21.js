e432da7338366c4b645f1ac315e63c8a427b2a5b
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-04-01 15:19:19 +0000

    Add 21.js

---
+++ src/21.js
@@ -0,0 +1,13 @@
+// Question 1: Calculate the sum of numbers from 0 to 5
+
+let total = Math.floor((5 * (5 + 1)) / 2);
+console.log(total);
+
+// Question 2: Print a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+
+// Question 3: Convert string 'hello world' to uppercase
+
+let helloWorldUppercase = "HELLO WORLD".toUpperCase();
+console.log(helloWorldUppercase);
