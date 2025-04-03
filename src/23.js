c1559f309b3d98ebf7d11bccf76dcd5cd82c6ced
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-04-03 16:57:45 +0000

    Add 23.js

---
+++ src/23.js
@@ -0,0 +1,8 @@
+function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage:
+console.log(getRandomInteger(0, 100));
