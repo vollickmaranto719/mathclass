4c3b2a34f9dd88243955f3a956e691d61a8cef7e
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-04-11 11:19:15 +0000

    Add 29.js

---
+++ src/29.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+let randomNumber = getRandomInt(10) + getRandomInt(5); // Generate a random number between 10 and 15
+console.log(randomNumber);
