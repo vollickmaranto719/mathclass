bbd7fe792c4657c846aceb9afb1a9bbe424e4a78
vollickmaranto719 <vollickmaranto719@gmail.com>
2025-04-24 04:54:50 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,8 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+
+const maxNumber = 100;
+console.log(getRandomInt(maxNumber)); // Output: random number between 0 and maxNumber - 1
